c4bd81f30de319d48fe179b3393e6fa61c822372
belya <belya@DESKTOP-9G92SH1>
2024-01-22 11:46:50 +0700

    Добавьте файлы проекта.

---
+++ Legion/MainWindow.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Legion
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ClickHandler(object sender, RoutedEventArgs args)
+        {
+
+        }
+    }
+}
