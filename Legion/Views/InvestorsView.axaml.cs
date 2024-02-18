d9dec4c59469238244ed7e60bce4dda961f37d44
belya <belya@DESKTOP-9G92SH1>
2024-02-18 17:22:59 +0700

    New investor form

--- Legion/Views/InvestorsView.axaml.cs
+++ Legion/Views/InvestorsView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System;
 
 namespace Legion;
 
@@ -9,5 +10,33 @@ public partial class InvestorsView : Window
     public InvestorsView()
     {
         InitializeComponent();
+    }
+
+    private void AddNewInvestor(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        SPAddNewInvestor.IsVisible = true;
+        BtnBack.IsVisible = true;
+        BtnAddNew.IsVisible = false;
+        SPInvestorDG.IsVisible = false;
+        SVLeft.IsPaneOpen = false;
+        SerachBox.IsVisible = false;
+        SerachBtn.IsVisible = false;
+        SpTriggerBtn.IsVisible = false;
+    }
+
+    private void BackBtnClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        SPAddNewInvestor.IsVisible = false;
+        BtnBack.IsVisible = false;
+        BtnAddNew.IsVisible = true;
+        SPInvestorDG.IsVisible = true;
+        SerachBox.IsVisible = true;
+        SerachBtn.IsVisible = true;
+        SpTriggerBtn.IsVisible = true;
+    }
+
+    private void TriggerPaneCommand(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        SVLeft.IsPaneOpen = !SVLeft.IsPaneOpen;
     }
 }
