0fb25420ad4eef076e3315e85a0556b1c40bc429
Samars <samarauss@outlook.com>
2024-02-21 04:06:04 +0700

    feat: context menu

--- Legion/ViewModels/InvestorsViewModel.cs
+++ Legion/ViewModels/InvestorsViewModel.cs
@@ -5,11 +5,14 @@ using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Legion.ViewModels
 {
@@ -18,10 +21,16 @@ namespace Legion.ViewModels
         private ApplicationDbContext _context;
         private bool _isPaneOpen;
 
+        public InvestorsViewModel()
+        {
+            _context = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>());
+        }
+
         public InvestorsViewModel(IScreen hostScreen, ApplicationDbContext context)
         {
             _context = context;
             _isPaneOpen = false;
+            _context.Investors.Load();
 
             PaneCommand = ReactiveCommand.Create(() =>
             {
@@ -32,9 +41,28 @@ namespace Legion.ViewModels
             {
                 hostScreen.Router.Navigate.Execute(new AddInvestorViewModel(hostScreen, context));
             });
+
+            DataGridEditActionCommand = ReactiveCommand.Create((Investor inv) =>
+            {
+                Debug.WriteLine(inv.ToString());
+            });
+
+
+            DataGridEditActionCommand = ReactiveCommand.Create((Investor inv) =>
+            {
+                Debug.WriteLine(inv.ToString());
+            });
+
+            DataGridRemoveActionCommand = ReactiveCommand.Create((Investor inv) =>
+            {
+                Debug.WriteLine(inv.Id.ToString() + "to remove");
+                _context.Investors.Remove(inv);
+                _context.SaveChangesAsync();
+                _context.Investors.LoadAsync();
+            });
         }
 
-        public List<Investor> Investors => _context.Investors.ToList();
+        public ObservableCollection<Investor> Investors => _context.Investors.Local.ToObservableCollection();
 
         public override IScreen HostScreen => throw new NotImplementedException();
         public bool IsPaneOpen
@@ -45,5 +73,8 @@ namespace Legion.ViewModels
 
         public ReactiveCommand<Unit, Unit> PaneCommand { get;}
         public ReactiveCommand<Unit, Unit> NewInvestorCommand { get; }
+        public ReactiveCommand<Investor, Unit> DataGridPrintActionCommand { get; set; }
+        public ReactiveCommand<Investor, Unit> DataGridEditActionCommand { get; set; }
+        public ReactiveCommand<Investor, Unit> DataGridRemoveActionCommand { get; set; }
     }
 }
