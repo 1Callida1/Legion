c38349ff6e3ff44b4ac69a088e734c4caa61f0a2
belya <belya@DESKTOP-9G92SH1>
2024-03-27 15:04:40 +0700

    fix conflict

---
+++ Legion/Helpers/ReportGenerator/ExcelGenerator.cs
@@ -0,0 +1,34 @@
+﻿using Legion.Models;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legion.Helpers.ReportGenerator
+{
+    public class ExcelGenerator
+    {
+        public byte[] Generate(ObservableCollection<Legion.Models.Contract> contracts = null, ObservableCollection<RenewalContract> renewalContracts = null, ObservableCollection<Investor> investors = null)
+        {
+            ExcelPackage package = new();
+
+            ExcelWorksheet sheet = package.Workbook.Worksheets.Add("Доход безнал");
+            sheet.Cells[8, 2].Value = "Имя инвестора:";
+            int row = 9;
+            int column = 2;
+            foreach (Investor investor in investors)
+            {
+                sheet.Cells[row, column].Value = investor.FirstName;
+                row++;
+            }
+
+            return package.GetAsByteArray();
+
+        }
+    }
+}
