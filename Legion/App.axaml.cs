10bab6a966fc27010a7f35ef8975ff52b6c8f58c
Samars <samarauss@outlook.com>
2024-04-06 03:23:13 +0700

    remote db

--- Legion/App.axaml.cs
+++ Legion/App.axaml.cs
@@ -15,6 +15,7 @@ using Legion.Models;
 using SkiaSharp;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using Legion.Helpers;
 using Legion.Models.Internal;
 using Legion.Views;
@@ -79,13 +80,13 @@ namespace Legion
                     _context.SaveChanges();
                     _context.Users.Add(new User() { Password = "123", UserName = "admin", EmployerFirstName = "Aboba", UserRole = _context.UserRoles.First(role => role.Role.Contains("Admin")) });
                     _context.Users.Add(new User() { Password = "321", UserName = "loh", EmployerFirstName = "Biba", UserRole = _context.UserRoles.First(role => role.Role.Contains("User")) });
-                    _context.ContractTypes.Add(new ContractType() { ContractIdFormat = "id/yy", Bet = 2, Formula = "x*p", Period = 6, TypeName = "Íàêîïèòåëüíûé", CanAddMoney = true });
-                    _context.ContractTypes.Add(new ContractType() { ContractIdFormat = "id/yy", Bet = 4, Formula = "x*p", Period = 12, TypeName = "Ãîäîâîé", CanAddMoney = false });
-                    _context.ContractTypes.Add(new ContractType() { ContractIdFormat = "id/yyyy/yy", Bet = 6, Formula = "x*p", Period = 36, TypeName = "Òðåõãîäîâîé", CanAddMoney = false });
-                    _context.ContractTypes.Add(new ContractType() { ContractIdFormat = "id/yyyy/yy", Bet = 7, Formula = "x*p", Period = 18, TypeName = "Äîõîäíûé", CanAddMoney = false });
-                    _context.ContractStatuses.Add(new ContractStatus() { Status = "Îòêðûò" });
-                    _context.ContractStatuses.Add(new ContractStatus() { Status = "Çàêðûò" });
-                    _context.ContractStatuses.Add(new ContractStatus() { Status = "Ïðèîñòàíîâëåí" });
+                    _context.ContractTypes.Add(new ContractType() { ContractIdFormat = "id/yy", Bet = 2, Formula = "x*p", Period = 6, TypeName = "Накопительный", CanAddMoney = true });
+                    _context.ContractTypes.Add(new ContractType() { ContractIdFormat = "id/yy", Bet = 4, Formula = "x*p", Period = 12, TypeName = "Годовой", CanAddMoney = false });
+                    _context.ContractTypes.Add(new ContractType() { ContractIdFormat = "id/yyyy/yy", Bet = 6, Formula = "x*p", Period = 36, TypeName = "Трехгодовой", CanAddMoney = false });
+                    _context.ContractTypes.Add(new ContractType() { ContractIdFormat = "id/yyyy/yy", Bet = 7, Formula = "x*p", Period = 18, TypeName = "Доходный", CanAddMoney = false });
+                    _context.ContractStatuses.Add(new ContractStatus() { Status = "Открыт" });
+                    _context.ContractStatuses.Add(new ContractStatus() { Status = "Закрыт" });
+                    _context.ContractStatuses.Add(new ContractStatus() { Status = "Приостановлен" });
                     _context.SaveChanges();
                 }
                 else
